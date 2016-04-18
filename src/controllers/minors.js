5e6d87024f7518ec17ef182a98b41f066a209077
Brett Lamy <bel423@me.com>
2016-04-17 22:45:30 -0400

    minors styling

--- src/controllers/minors.js
+++ src/controllers/minors.js
@@ -4,9 +4,11 @@ define(['model/minors'], function(model) {
   model.subscribe(function(payload) {
     var html = payload.reduce(function(prev, curr) {
       return prev + `
-        <div class='${curr.name}'>
-          <i class='fa ${curr.name} fa-3x'></i>
-          <p>${curr.title}</p>
+        <div class='${curr.name} hoverShadow3dp'>
+          <div class='minor-wrapper'>
+            <i class='fa ${curr.name} fa-3x'></i>
+            <p>${curr.title}</p>
+          </div>
         </div>`;
     }, '');
 
